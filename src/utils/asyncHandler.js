0448721566d3bac56dfe5d5776f2c56c866059f6
Parveenrps <166019586+Parveenrps@users.noreply.github.com>
2025-11-25 09:49:25 +0000

    custom api and error handlers added in utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+//promises
+const asyncHandler = (requestHandler) => {
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).reject((error)=>next(error))
+    }
+
+}
+
+export {asyncHandler}
+
+
+//in try catch
+// const asyncHandler = (fn) = async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message:error.message
+//         })
+//     }
+
+// }
