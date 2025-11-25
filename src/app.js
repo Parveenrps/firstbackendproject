e92c1f464b33b119b802179f3b0362120c45d2a0
Parveenrps <166019586+Parveenrps@users.noreply.github.com>
2025-11-25 07:30:28 +0000

    express config added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true      
+}))
 
+//for form data
+app.use(express.json({limit: "16kb"}));
+//for url
+app.use(express.urlencoded({extended: true, limit:"16kb"}));
+//for public assets
+app.use(express.static("public"));
+
+//cookie-parser
+app.use(cookieParser());
 
 export {app}; 
