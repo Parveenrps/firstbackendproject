b1e3a4e343f3b9830829204591a0b5926e2a1c49
Parveenrps <166019586+Parveenrps@users.noreply.github.com>
2025-12-08 12:29:20 +0000

    getUserChannelProfile and aggregation pipeline added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -386,6 +386,83 @@ const updateUserCoverImage = asyncHandler(async(req, res)=>{
 
 
 
+});
+
+const getUserChannelProfile = asyncHandler(async(req, res)=>{
+    const {username} = req.params;
+
+    if(!username){
+        throw new ApiError(400, "username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel", //channel ko select karenge to subs milenge
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subcriber", //subscriber ko select karenge to channel milenge jo subscribe kiye hue hai
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subcribedTo"
+                },
+                isSubscribed: {
+                    $cond:{
+                        if: { 
+                            $in: [req.user?._id, "$subscribers.subscriber"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {   //saari values nhi dega only projected value dega
+            $project:{
+                fullName: 1,
+                username: 1,
+                subscriberCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exist");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            channel[0],
+            "User channel does fetched successfully"
+        )
+    )
 })
 export {
     registerUser,
