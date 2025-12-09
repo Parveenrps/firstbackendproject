b9094b0cbbe6a31b9b007ab99ea536a86d146d1a
Parveenrps <166019586+Parveenrps@users.noreply.github.com>
2025-12-09 05:47:50 +0000

    watch history controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -514,6 +514,16 @@ const getWatchHistory = asyncHandler(async(req, res)=>{
             }
         }
     ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "watch history fetched successfully"
+        )
+    )
 })
 
 export {
